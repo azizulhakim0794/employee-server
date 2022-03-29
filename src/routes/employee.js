d80c8e6bf9ddc1528efdf88a83d914211353f712
azizulhakim0794 <azizulhakimtumzid0793@gmail.com>
2022-03-29 16:04:50 +0600

    cors fix

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cors = require('cors')
+const app = express();
+app.use(cors())
 const router = express.Router();
 const employeeController = require('./../employee/employee.controller')
 router.post('/',employeeController.createPost)
